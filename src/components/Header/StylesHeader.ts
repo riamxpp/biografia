6ee724703161279ec4c7c89757e473660b14c328
riamxpp <riamstefeson616@gmail.co>
2022-05-24 09:12:56 -0300

    Corrigindo detalhes do botão do Header

--- src/components/Header/StylesHeader.ts
+++ src/components/Header/StylesHeader.ts
@@ -12,11 +12,15 @@ const ButtonHeader = styled.button`
   height: 2rem;
   width: 2rem;
   background: ${colors.backgroundColor};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
   &:after {
     content: "";
     display: inline-block;
     height: 2px;
-    width: 1.5rem;
+    width: 2rem;
     background: #fff;
     box-shadow: 0 8px #fff, 0 -8px #fff;
   }
